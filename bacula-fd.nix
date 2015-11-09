767e157bbd722ea5193b177df39e2c0df76dd0d3
Alex Brandt <alunduil@alunduil.com>
2015-11-08 18:13:22 -0600

    fix fd.name

--- bacula-fd.nix
+++ bacula-fd.nix
@@ -4,7 +4,7 @@
   imports = [];
 
   services.bacula-fd.enable = true;
-  services.bacula-fd.director.name = config.networking.hostName + "-fd";
+  services.bacula-fd.name = config.networking.hostName + "-fd";
 
   services.bacula-fd.director."dornick.alunduil.com".password = "NrXXn/7ywN+DNmRxyL40+DVAe8x0oXRxz/ulsHL347Fc";
 }
