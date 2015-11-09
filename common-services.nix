1e1051eec6d02db9f25267fc57de60d2749c5907
Alex Brandt <alunduil@alunduil.com>
2015-11-08 19:17:36 -0600

    fix typo in smard parameters

--- common-services.nix
+++ common-services.nix
@@ -14,8 +14,11 @@
   services.smartd.enable = true;
   services.smartd.notifications.mail.enable = true;
 
-  services.smartd.devicse = [ { device = "/dev/sda"; } { device = "/dev/sdb"; } ];
   services.smartd.defaults.monitored = "-a -o on -s (S/../.././02|L/../../7/04)";
+  services.smartd.devices = [
+    { device = "/dev/sda"; }
+    { device = "/dev/sdb"; }
+  ];
 
   services.uptimed.enable = true;
 }
