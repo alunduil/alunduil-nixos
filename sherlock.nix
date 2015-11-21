49303ba563ed994304e305ca608d4c7eca3120d8
Alex Brandt <alunduil@alunduil.com>
2015-11-20 20:24:36 -0600

    add IP information to sherlock

--- sherlock.nix
+++ sherlock.nix
@@ -8,6 +8,16 @@
 
   hardware.cpu.amd.updateMicrocode = true;
 
-  networking.hostName = "sherlock.alunduil.com";
   time.timeZone = "America/Chicago";
+
+  networking.hostName = "sherlock.alunduil.com";
+
+  networking.nameservers = [
+    "8.8.8.8"
+    "8.8.4.4"
+  ];
+
+  networking.defaultGateway = "64.68.174.1";
+
+  networking.interfaces.enp0s7.ip4 = [ { address = "64.68.174.56"; prefixLength = 32; } ];
 }
