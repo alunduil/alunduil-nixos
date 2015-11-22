611cbee4435e0cec32795572007ca234ab52d660
Alex Brandt <alunduil@alunduil.com>
2015-11-22 09:05:07 -0600

    add static route to get to giskard
    Thanks NVC for being completely useless.

--- sherlock.nix
+++ sherlock.nix
@@ -20,4 +20,6 @@
   networking.defaultGateway = "64.68.174.1";
 
   networking.interfaces.enp0s7.ip4 = [ { address = "64.68.174.56"; prefixLength = 24; } ];
+
+  networking.localCommands = "ip route add 64.68.174.55 via 64.68.174.1";  # Thanks NVC
 }
