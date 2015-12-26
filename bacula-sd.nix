c7de33e6641c88c7d268d778deede7ab2db9377d
Alex Brandt <alunduil@alunduil.com>
2015-12-25 23:00:24 -0600

    add USB-RAID1 storage device

--- bacula-sd.nix
+++ bacula-sd.nix
@@ -7,4 +7,14 @@
   services.bacula-sd.name = config.networking.hostName + "-sd";
 
   services.bacula-sd.director."mycroft.alunduil.com-dir".password = "ab21917b042b43ae996534179bb32eba";
+
+  services.bacula-sd.device."USB-RAID1" = {
+    archiveDevice = "/var/backups";
+    mediaType = "USB-RAID1-882e244d128a40ffaa0c85b4f58457f2";
+    extraDeviceConfig = ''
+    Device Type = File
+    Random Access = yes
+    Requires Mount = no
+    '';
+  };
 }
