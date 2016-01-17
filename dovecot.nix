0bcbbed35f6cd2e7b421296e56ec03caeabfe17d
Alex Brandt <alunduil@alunduil.com>
2016-01-17 11:08:11 -0600

    allow traffic for IMAP

--- dovecot.nix
+++ dovecot.nix
@@ -3,6 +3,8 @@
 {
   imports = [];
 
+  networking.firewall.allowedTCPPorts = [ 143 ];
+
   services.dovecot2.enable = true;
   services.dovecot2.enableImap = true;
   services.dovecot2.enablePop3 = false;
