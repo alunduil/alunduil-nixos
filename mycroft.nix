bc1384288678772293118654a94b9ad2b05b92cd
Alex Brandt <alunduil@alunduil.com>
2016-01-24 14:56:39 -0600

    update mycroft IP

--- mycroft.nix
+++ mycroft.nix
@@ -22,7 +22,7 @@
 
   networking.defaultGateway = "64.68.174.1";
 
-  networking.interfaces.enp0s7.ip4 = [ { address = "64.68.174.57"; prefixLength = 24; } ];
+  networking.interfaces.enp0s7.ip4 = [ { address = "64.68.174.54"; prefixLength = 24; } ];
 
-  networking.localCommands = "ip route add 64.68.174.55 via 64.68.174.1";  # Thanks NVC
+  networking.localCommands = "ip route add 64.68.174.56 via 64.68.174.1";  # Thanks NVC
 }
