ceba0fe57371932d2c4782f373b5eeea6c38a795
Alex Brandt <alunduil@alunduil.com>
2017-06-03 09:54:33 -0500

    factor all cronus hosts to profile

--- hosts/hestia.nix
+++ hosts/hestia.nix
@@ -1,12 +1,13 @@
 { ... }:
 { imports =
-    [ ../../system.nix
+    [ ../../profiles/cronus
+      ../../system.nix
     ];
 
   environment.noXlibs = true;
 
   networking =
-    { hostName = "hades";
+    { hostName = "hestia";
     };
 
   sound.enable = false;
--- hosts/zeus.nix
+++ hosts/zeus.nix
@@ -1,12 +1,13 @@
 { ... }:
 { imports =
-    [ ../../system.nix
+    [ ../../profiles/cronus
+      ../../system.nix
     ];
 
   environment.noXlibs = true;
 
   networking =
-    { hostName = "hestia";
+    { hostName = "zeus";
     };
 
   sound.enable = false;
