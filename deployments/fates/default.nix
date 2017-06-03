b3e2e32f7dc2c93fd73e72c42d7a96610d9e38cf
Alex Brandt <alunduil@alunduil.com>
2017-06-03 10:07:59 -0500

    fix import paths

--- deployments/fates/default.nix
+++ deployments/fates/default.nix
@@ -2,7 +2,7 @@
   
   fates    = import ../../hosts/fates;
 
-  atropos  = import ../../hosts/atropos;
-  clothio  = import ../../hosts/clothio;
-  lachesis = import ../../hosts/lachesis;
+  atropos  = import ../../hosts/atropos.nix;
+  clothio  = import ../../hosts/clothio.nix;
+  lachesis = import ../../hosts/lachesis.nix;
 }
