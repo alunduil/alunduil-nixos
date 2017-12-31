8343cc508afd702f8745c962bbc832529cb37a70
Alex Brandt <alunduil@alunduil.com>
2017-12-30 23:29:45 -0600

    Revert "integrate nginx configuration"
    This reverts commit 7494816af02e808f6ed48b30bce180d96b0b1416.

--- hosts/fates/nginx/default.nix
+++ hosts/fates/nginx/default.nix
@@ -12,21 +12,7 @@
     ];
 
   services.nginx =
-    { appendHttpConfig = ''
-      upstream fates {
-        server atropos:443;
-        server clothio:443;
-        server lachesis:443;
-      }
-
-      server {
-        listen 80;
-
-        location / {
-          proxy_pass https://fates;
-        }
-      }
-      ''
+    { config = builtins.readFile ./nginx.conf;
       enable = true;
       statusPage = true;
     };
