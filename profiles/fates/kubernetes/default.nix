fdf310fb5db469a000be96426391241117f76928
Alex Brandt <alunduil@alunduil.com>
2017-12-30 19:07:25 -0600

    add apiserver configuration

--- profiles/fates/kubernetes/default.nix
+++ profiles/fates/kubernetes/default.nix
@@ -1,6 +1,25 @@
 { config, ... }:
 { services.kubernetes =
-    { caFile = ./certificates/ca.pem;
+    { apiserver =
+        { kubeletClientCertFile = ./certificates + "/${config.networking.hostName}.pem";
+          kubeletClientKeyFile = ./certificates + "/${config.networking.hostName}-key.pem";
+          serviceAccountKeyFile = ./certificates/ca-key.pem;
+          tlsCertFile = ./certificates + "/${config.networking.hostName}.pem";
+          tlsKeyFile = ./certificates + "/${config.networking.hostName}-key.pem";
+        };
+      
+      caFile = ./certificates/ca.pem;
+
+      etcd =
+        { caFile = ../etcd/certificates/clients/ca.pem;
+          certFile = ../etcd/certificates/clients + "/${config.networking.hostName}.pem";
+          keyFile = ../etcd/certificates/clients + "/${config.networking.hostName}-key.pem";
+          servers =
+            [ "https://atropos:2379"
+              "https://clothio:2379"
+              "https://lachesis:2379"
+            ];
+        };
 
       kubeconfig =
         { certFile = ./certificates + "/${config.networking.hostName}.pem";
--- profiles/kubernetes/kubernetes.nix
+++ profiles/kubernetes/kubernetes.nix
@@ -4,7 +4,26 @@
     ];
 
   services.kubernetes =
-    { apiserver.enable = true;
+    { apiserver =
+        { address = "0.0.0.0";
+          admissionControl =
+            [ DefaultStorageClass
+              Initializers
+              LimitRanger
+              NamespaceLifecycle
+              NodeRestriction
+              ResourceQuota
+              ServiceAccount
+            ];
+          advertiseAddress = config.networking.hostName;
+          authorizationMode =
+            [ "Node"
+              "RBAC"
+            ];
+          enable = true;
+          runtimeConfig = "api/all";
+        };
+
       controllerManager.enable = true;
       kubelet.enable = true;
       proxy.enable = true;
