bedb0fee68bb6d70b8a08ac9c0bf6dd62f5bdce3
Alex Brandt <alunduil@alunduil.com>
2018-03-30 21:25:04 -0500

    remove all sherlock references
    I'm not removing the orphaned user perniferuse in case I want to use it
    elsewhere later but this simplifies the configuration a bit.

--- hosts/sherlock/bacula-fd.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{ imports =
-    [ ../../services/bacula-fd.nix
-    ];
-
-  networking.firewall.extraCommands = ''
-  iptables -A nixos-fw -s 64.68.174.54 -p tcp --dport 9102 -j nixos-fw-accept
-  '';
-}
