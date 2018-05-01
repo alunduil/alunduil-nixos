329a0599281c912f6d65702dc2efcfac3ec22f2f
Alex Brandt <alunduil@alunduil.com>
2018-05-01 08:46:14 -0700

    update elijah settings with California changes

--- hosts/elijah/postfix/default.nix
+++ hosts/elijah/postfix/default.nix
@@ -6,6 +6,7 @@
   services.postfix =
     { extraConfig = builtins.readFile ./main.cf;
       hostname = pkgs.lib.mkForce "elijah.laptops.alunduil.com";
+      relayHost = pkgs.lib.mkForce "mail.alunduil.com:587";
       mapFiles =
         { "sasl_passwords" = ./sasl_passwords;
         };
