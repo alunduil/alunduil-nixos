46631ae1b2e7f834db328d63a793d9d8c67a9243
Alex Brandt <alunduil@alunduil.com>
2018-05-01 08:55:02 -0700

    set elijah's relay port to 587

--- hosts/elijah/postfix/default.nix
+++ hosts/elijah/postfix/default.nix
@@ -6,7 +6,7 @@
   services.postfix =
     { extraConfig = builtins.readFile ./main.cf;
       hostname = pkgs.lib.mkForce "elijah.laptops.alunduil.com";
-      relayHost = pkgs.lib.mkForce "mail.alunduil.com:587";
+      relayPort = 587;
       mapFiles =
         { "sasl_passwords" = ./sasl_passwords;
         };
