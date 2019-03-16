ca493dbd47a05f1913c97f413de43b7c060dcec2
Alex Brandt <alunduil@gmail.com>
2019-03-15 20:35:55 -0700

    Add broadcast namespace to smart options for nvme.

--- services/smartd.nix
+++ services/smartd.nix
@@ -1,12 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 let defaults = "-a -o on -s (S/../.././02|L/../../7/04) -T permissive";
 in
   {
+    environment.systemPackages = [
+      pkgs.smartmontools
+    ];
+
     services.smartd = {
       defaults.autodetected = defaults;
       defaults.monitored = defaults;
       devices = [
-        { device = "/dev/nvme0n1"; }
+        {
+          device = "/dev/nvme0n1";
+          options = "-d nvme,0xffffffff";
+        }
       ];
       enable = true;
       notifications.mail.enable = true;
